d661112e450141c76026a58ba195d2f87dd990ad
Zeronian <93339174+Zeronian@users.noreply.github.com>
2021-03-04 08:24:37 +0000

    icon changes

---
+++ app/src/androidTest/java/com/myapp/rishabhrawat/cryptocurrencytracker/ExampleInstrumentedTest.kt
@@ -0,0 +1,15 @@
+
+package com.myapp.rishabhrawat.cryptocurrencytracker
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
