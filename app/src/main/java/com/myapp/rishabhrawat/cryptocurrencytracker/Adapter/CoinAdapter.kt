a84324104590fc133e4875057198f6ee19b89abd
Zeronian <93339174+Zeronian@users.noreply.github.com>
2021-03-14 09:50:42 +0000

    adding script

--- app/src/main/java/com/myapp/rishabhrawat/cryptocurrencytracker/Adapter/CoinAdapter.kt
+++ app/src/main/java/com/myapp/rishabhrawat/cryptocurrencytracker/Adapter/CoinAdapter.kt
@@ -30,3 +30,35 @@ class CoinViewHolder(itemView:View): RecyclerView.ViewHolder(itemView){
 }
 
 class CoinAdapter(recyclerView: RecyclerView,internal var activity: Activity,var items:List<CoinModel>): RecyclerView.Adapter<CoinViewHolder>()
+{
+
+    internal var loadMore:ILoadMore?=null
+    var isLoading:Boolean=false
+    var visibleThreshold=5
+    var lastVisibleItem:Int=0
+    var totalItemCount:Int=0
+
+    init {
+        val linearLayout=recyclerView.layoutManager as LinearLayoutManager
+        recyclerView.addOnScrollListener(object :RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView?, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                totalItemCount=linearLayout.itemCount
+                lastVisibleItem=linearLayout.findLastVisibleItemPosition()
+                if(!isLoading && totalItemCount<=lastVisibleItem+visibleThreshold )
+                {
+                    if(loadMore!=null)
+                        loadMore!!.onLoadMore()
+                    isLoading=true
+                }
+            }
+        })
+    }
+
+    fun setLoadMore(loadMore: ILoadMore)
+    {
+        this.loadMore=loadMore
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinViewHolder {
+        val view=LayoutInflater.from(activity).inflate(R.layout.coin_layout,parent,false)
