cbf1eee53f9d0a162d759f9b9f7056fe264ee560
Zeronian <93339174+Zeronian@users.noreply.github.com>
2021-04-03 22:11:07 +0000

    initial committ

--- app/src/main/java/com/myapp/rishabhrawat/cryptocurrencytracker/Common/Common.kt
+++ app/src/main/java/com/myapp/rishabhrawat/cryptocurrencytracker/Common/Common.kt
@@ -1,4 +1,6 @@
 package com.myapp.rishabhrawat.cryptocurrencytracker.Common
 
 object Common {
-    var imageurl="https://res.cloudinary.com/dxi90ksom/image
+    var imageurl="https://res.cloudinary.com/dxi90ksom/image/upload/"
+
+    var MAX_COIN_LOAD=10
