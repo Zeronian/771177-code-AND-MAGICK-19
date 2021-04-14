27835b3d91db3636ed89bcac6ae29e1837e61f4e
Zeronian <93339174+Zeronian@users.noreply.github.com>
2021-04-14 08:56:22 +0000

    Added translation using Weblate (Dutch)

--- app/src/main/java/com/myapp/rishabhrawat/cryptocurrencytracker/Common/Common.kt
+++ app/src/main/java/com/myapp/rishabhrawat/cryptocurrencytracker/Common/Common.kt
@@ -3,4 +3,5 @@ package com.myapp.rishabhrawat.cryptocurrencytracker.Common
 object Common {
     var imageurl="https://res.cloudinary.com/dxi90ksom/image/upload/"
 
-    var MAX_COIN_LOAD=10
+    var MAX_COIN_LOAD=1000
+}
