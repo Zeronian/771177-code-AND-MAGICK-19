fc42b19acc973e5dce5d743cb0d7fcbf53a73221
Zeronian <93339174+Zeronian@users.noreply.github.com>
2022-07-09 12:20:05 +0000

    Disable travis email notifications

--- app/src/test/java/com/myapp/rishabhrawat/cryptocurrencytracker/ExampleUnitTest.kt
+++ app/src/test/java/com/myapp/rishabhrawat/cryptocurrencytracker/ExampleUnitTest.kt
@@ -5,4 +5,5 @@ import org.junit.Test
 import org.junit.Assert.*
 
 /**
- * Example local unit test, which will execute on
+ * Example local unit test, which will execute on the development machine (host).
+ *
