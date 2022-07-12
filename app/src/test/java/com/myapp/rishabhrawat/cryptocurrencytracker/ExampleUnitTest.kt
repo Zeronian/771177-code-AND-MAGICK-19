9764a836d71bd51713d2d2867f479de20223b2cf
Zeronian <93339174+Zeronian@users.noreply.github.com>
2022-07-12 20:31:07 +0000

    Publishing updated documentation

--- app/src/test/java/com/myapp/rishabhrawat/cryptocurrencytracker/ExampleUnitTest.kt
+++ app/src/test/java/com/myapp/rishabhrawat/cryptocurrencytracker/ExampleUnitTest.kt
@@ -7,4 +7,6 @@ import org.junit.Assert.*
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
- * See [testing documentation](http://d.android.com/t
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitT
